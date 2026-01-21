068616498238ffedede8fd618c2816a00be19208
Nbelman1 <nbelman1@gmail.com>
2026-01-21 15:25:48 -0500

    Added useState hook, passed props to InterfaceTabs

--- chopscape/src/App.jsx
+++ chopscape/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 import './App.css';
+import './components/UserInterface/InterfaceTabs/interface-styles.css';
+import InterfaceTabs from './components/UserInterface/InterfaceTabs/InterfaceTabs';
 
 function App() {
 
+  const [activeTab, setActiveTab] = useState("combat");
+
   return (
     <>
-      
+      <InterfaceTabs activeTab={activeTab} setActiveTab={setActiveTab}/>
     </>
   )
 }
