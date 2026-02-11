11edddd159a823f9c9be4859aabb060f5ef602f8
Nbelman1 <nbelman1@gmail.com>
2026-02-11 14:25:14 -0500

    Changed time standard from seconds to milliseconds

--- chopscape/src/data/logs.js
+++ chopscape/src/data/logs.js
@@ -1,5 +1,5 @@
 // holds static data for trees
-// all times are measured in seconds
+// all times are measured in milliseconds (1000 ms = 1 second)
 
 export const LOGS = [
     { 
@@ -9,9 +9,9 @@ export const LOGS = [
         expGained: 25,
         harvestLimit: 1, 
         lifeTime: 0, // normal trees are felled from 1 successful chop
-        timeBetweenChops: 2.4, // roll successRate from chop-chance.js every chop
-        respawnTimeMin: 36, // normal tree respawn randomly between 36s and 60s
-        respawnTimeMax: 60, 
+        timeBetweenChops: 2400, // roll successRate from chop-chance.js every chop
+        respawnTimeMin: 36000, // normal tree respawn randomly between 36s and 60s
+        respawnTimeMax: 60000, 
         imagePath: "/images/tree.png"
     },
     { 
@@ -20,10 +20,10 @@ export const LOGS = [
         logType: "Oak logs",
         expGained: 37,
         harvestLimit: Infinity, // harvest based on successRate and lifeTime
-        lifeTime: 27,
-        timeBetweenChops: 2.4,
-        respawnTimeMin: 27, // oak trees respawn consistently every 27s
-        respawnTimeMax: 27, 
+        lifeTime: 27000,
+        timeBetweenChops: 2400,
+        respawnTimeMin: 27000, // consistent respawn time, every 27s
+        respawnTimeMax: 27000, 
         imagePath: "/images/oak-tree.png"
     },
     { 
@@ -32,10 +32,10 @@ export const LOGS = [
         logType: "Willow logs",
         expGained: 67,
         harvestLimit: Infinity,  // harvest based on successRate and lifeTime
-        lifeTime: 30,
-        timeBetweenChops: 2.4,
-        respawnTimeMin: 14, // consistent respawn time
-        respawnTimeMax: 14, 
+        lifeTime: 30000,
+        timeBetweenChops: 2400,
+        respawnTimeMin: 14000, // consistent respawn time, every 14s
+        respawnTimeMax: 14000, 
         imagePath: "/images/willow-tree.png"
     },
 ];
