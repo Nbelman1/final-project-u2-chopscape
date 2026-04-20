3c586a6217e1ab8ce17c9ef4a5f3dfeb16ccb366
Nbelman1 <116769408+Nbelman1@users.noreply.github.com>
2026-04-19 22:21:10 -0400

    Allowed access to all origins for testing

--- reactive-roots-backend/src/main/java/com/example/reactive_roots/controllers/UserController.java
+++ reactive-roots-backend/src/main/java/com/example/reactive_roots/controllers/UserController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*") // allow access to all origins
 public class UserController {
 
     UserRepository repository;
