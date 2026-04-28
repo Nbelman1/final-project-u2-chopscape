47f9398aa9f9b8808a0f09e695729b7b0a02dcb4
Nbelman1 <116769408+Nbelman1@users.noreply.github.com>
2026-04-27 22:17:53 -0400

    Added / to fix syntax error

--- reactive-roots-backend/src/main/java/com/example/reactive_roots/config/SecurityConfig.java
+++ reactive-roots-backend/src/main/java/com/example/reactive_roots/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
                         .requestMatchers("/error").permitAll()
                         .requestMatchers(HttpMethod.PUT, "/api/stats/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
-                        .requestMatchers(HttpMethod.GET, "api/users/exists/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/users/exists/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .build();
